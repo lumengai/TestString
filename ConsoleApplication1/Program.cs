854cdff431612c298f6aca3a7b960c3e15b19830
unknown <1280227259@qq.com>
2018-01-11 14:55:54 +0800

    first

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        /// <summary>
+        /// string 是特殊的引用类型：执行一次销毁一次，执行一万次销毁一万次
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            string str = "";
+            TestString ts = new TestString();
+
+            str = ts.TString("bbbb");
+
+            Console.WriteLine(str);
+        }
+
+    }
+}
